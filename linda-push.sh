2ac231af0dade84e15d54e216d94dc9d030513dc
Sam <sam@mageplaza.com>
2016-12-14 11:19:24 +0700

    clean up

--- linda-push.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-git pull && git add . && git commit -m "Linda: Update" && git push
--- sam-push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-msg=$1
-
-git pull && git add . && git commit -m "Sam: update $msg" && git push
